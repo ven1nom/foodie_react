09d6f9759f92170610f82ab0aaecee3ad0fa088e
ven1nom <your-email@example.com>
2024-09-10 20:42:34 +0530

    second_commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,25 @@
-import React from "react"
+import React,{useState} from "react"
 import ResCard from "./ResCard"
-import resObj from "../utils/mockData"
+import resList from "../utils/mockData"
+import RestaurantCard from "./ResCard";
 export default function Main()
 {
+    const [listOfRestaurants,setListOfRestaurants]=useState(resList)
+    
     return(
         <div className="main-section">
-             <button>Search</button>
+             <button onClick={()=>{
+                let filterList=listOfRestaurants.filter(
+                    (res)=> res.data.avgRating>3
+                );
+              // console.log(filterList)
+              setListOfRestaurants(filterList)
+             }}
+             >Search</button>
              <div className="main-section card-container">
-             <ResCard resData={resObj}/>
+             {listOfRestaurants.map((restaurant) => (
+          <ResCard key={restaurant.data.id} resData={restaurant} />
+        ))}
              </div>
     
         </div>  
--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -1,17 +1,38 @@
 import React from "react"
 import {CDN_URL} from "../utils/constant"
 
-export default function ResCard(props)
-{
-    const{name,cuisines,avgRating,sla:{deliveryTime},cloudinaryImageId}=props.resData
-    return(
-        <div className="res-card">
-            <img className="res-image"src={CDN_URL+cloudinaryImageId}/>
-            <h3>{name}</h3>
-            <h4>{cuisines.join(',')}</h4>
-            <h4><span>{avgRating}</span>stars</h4>
-            <h4><span>{deliveryTime}</span>minutes</h4>
-        </div>
-    )
+const ResCard = (props) => {
+    const { resData } = props;
 
-}
+    const {
+        cloudinaryImageId,
+        name,
+        cuisines,
+        avgRating,
+        costForTwo,
+        deliveryTime,
+    } = resData?.data;
+
+    return (
+        <div
+            className="res-card"
+            style={{
+                backgroundColor: '#f0f0f0',
+            }}
+        >
+            <img
+                className="res-logo"
+                src={CDN_URL + cloudinaryImageId}
+                alt="Biryani"
+            />
+
+            <h3>{name}</h3>
+            <h4>{cuisines.join(', ')}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>₹{costForTwo / 100} FOR TWO</h4>
+            <h4>{deliveryTime} minutes</h4>
+        </div>
+    );
+};
+
+export default ResCard;
